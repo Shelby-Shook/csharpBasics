4ade676a94183abbc01eff1c06d12165dfce42b5
Shelby Shook <shelby.shook1217@gmail.com>
2023-05-08 14:26:35 -0400

    Created first C# Project

---
+++ 00_HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// console.log("Hello, World!");
+
+// C# is a compiled language 
+// Javascript is interpreted 
+
+// Before running C#, we must build and compile it
+// dotnet build 
+// dotnet run --project 00_HelloWorld
+
+// dotnet watch provides a hot reload
+// run the application after changes are saved
+
+// ctrl+c to stop dotnet watch
+
+Console.WriteLine("My name is Sheebs!");
